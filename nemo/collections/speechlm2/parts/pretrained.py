60a4ef696676ebe1a04ed187e66a63ff2bb3a9ba
Edresson Casanova <edresson1@gmail.com>
2025-05-15 12:11:50 -0700

    Add pretrained_tts parameter

--- nemo/collections/speechlm2/parts/pretrained.py
+++ nemo/collections/speechlm2/parts/pretrained.py
@@ -17,7 +17,7 @@ from pathlib import Path
 import torch
 from peft import PeftModel
 from transformers import AutoConfig, AutoModelForCausalLM
-
+from nemo.utils import logging
 
 def load_pretrained_nemo(cls, model_path_or_name: str):
     """
@@ -58,3 +58,17 @@ def move_embedding(model):
         del model.llm.base_model.model.model.embed_tokens
     else:
         del model.llm.model.embed_tokens
+
+
+def set_model_dict_for_partial_init(pretrained_dict, model_dict):
+    # 1. filter out different size layers
+    for k, v in list(pretrained_dict.items()):
+        if k in model_dict and hasattr(model_dict[k], "numel") and v.numel() != model_dict[k].numel():
+            del pretrained_dict[k]
+            logging.info(" | > Layer with shape mismatach in the model definition: {}".format(k)) 
+    # 2. filter out unnecessary keys
+    pretrained_dict = {k: v for k, v in pretrained_dict.items() if k in model_dict}
+    # 3. overwrite entries in the existing state dict
+    model_dict.update(pretrained_dict)
+    logging.info(" | > {} / {} layers are restored.".format(len(pretrained_dict), len(model_dict)))
+    return model_dict
